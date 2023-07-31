c63657f1c82db7947209f072de6144a916335647
Umair Khan <125777604+Umair-Web@users.noreply.github.com>
2023-07-31 17:23:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,193 @@
+let menu1=document.getElementById("menu-btn");
+menu1.addEventListener("click",print)
+function print(){
+    var shift=document.getElementById("nav");
+    console.log("Hello");
+    // shift.style.left="5%";
+    if(shift.style.left=="5%"){
+        shift.style.left="-100%";
+       
+
+    }
+    else{
+        shift.style.left="5%"
+    }
+    // shift.style.left="5%";
+
+    
+}
+// COCA COLA
+let cocacola=document.getElementById("Coco-Cola");
+cocacola.addEventListener("click",coca)
+function coca(){
+    document.body.style.backgroundColor = "#ad7575";
+
+    let bubble1=document.getElementById("allbubbles1");
+    bubble1.style.backgroundColor="#862828";
+
+    let bubble2=document.getElementById("allbubbles2");
+    bubble2.style.backgroundColor="#862828";
+
+    let bubble3=document.getElementById("allbubbles3");
+    bubble3.style.backgroundColor="#862828";
+
+    let bubble4=document.getElementById("allbubbles4");
+    bubble4.style.backgroundColor="#862828";
+
+    let bubble5=document.getElementById("allbubbles5");
+    bubble5.style.backgroundColor="#862828";
+
+    let bubble6=document.getElementById("allbubbles6");
+    bubble6.style.backgroundColor="#862828";
+
+    let bubble7=document.getElementById("allbubbles7");
+    bubble7.style.backgroundColor="#862828";
+
+    let bubble8=document.getElementById("allbubbles8");
+    bubble8.style.backgroundColor="#862828";
+
+    let bubble9=document.getElementById("allbubbles9");
+    bubble9.style.backgroundColor="#862828";
+
+    let bubble10=document.getElementById("allbubbles10");
+    bubble10.style.backgroundColor="#862828";
+
+    let logobackground=document.getElementById("fancyborder");
+    logobackground.style.backgroundColor="#b22929";
+    let logo=document.getElementById("canLogo");
+    logo.src="images/coca-cola-2.png";
+    let can=document.getElementById("canImage");
+    can.src="images/coca can.png";
+}
+// PEPSI
+let pepsi=document.getElementById("Pepsi");
+pepsi.addEventListener("click",pep)
+function pep(){
+    document.body.style.backgroundColor = "#7587ad";
+
+    let bubble1=document.getElementById("allbubbles1");
+    bubble1.style.backgroundColor="#284386";
+
+    let bubble2=document.getElementById("allbubbles2");
+    bubble2.style.backgroundColor="#284386";
+
+    let bubble3=document.getElementById("allbubbles3");
+    bubble3.style.backgroundColor="#284386";
+
+    let bubble4=document.getElementById("allbubbles4");
+    bubble4.style.backgroundColor="#284386";
+
+    let bubble5=document.getElementById("allbubbles5");
+    bubble5.style.backgroundColor="#284386";
+
+    let bubble6=document.getElementById("allbubbles6");
+    bubble6.style.backgroundColor="#284386";
+
+    let bubble7=document.getElementById("allbubbles7");
+    bubble7.style.backgroundColor="#284386";
+
+    let bubble8=document.getElementById("allbubbles8");
+    bubble8.style.backgroundColor="#284386";
+
+    let bubble9=document.getElementById("allbubbles9");
+    bubble9.style.backgroundColor="#284386";
+
+    let bubble10=document.getElementById("allbubbles10");
+    bubble10.style.backgroundColor="#284386";
+
+    let logobackground=document.getElementById("fancyborder");
+    logobackground.style.backgroundColor="#294bb2";
+    let logo=document.getElementById("canLogo");
+    logo.src="images/pepsilogo.png";
+    let can=document.getElementById("canImage");
+    can.src="images/pespi can.png";
+}
+
+// Fanta
+let fanta=document.getElementById("Fanta");
+fanta.addEventListener("click",fan)
+function fan(){
+    document.body.style.backgroundColor = "#be9757";
+
+    let bubble1=document.getElementById("allbubbles1");
+    bubble1.style.backgroundColor="#864c28";
+
+    let bubble2=document.getElementById("allbubbles2");
+    bubble2.style.backgroundColor="#864c28";
+
+    let bubble3=document.getElementById("allbubbles3");
+    bubble3.style.backgroundColor="#864c28";
+
+    let bubble4=document.getElementById("allbubbles4");
+    bubble4.style.backgroundColor="#864c28";
+
+    let bubble5=document.getElementById("allbubbles5");
+    bubble5.style.backgroundColor="#864c28";
+
+    let bubble6=document.getElementById("allbubbles6");
+    bubble6.style.backgroundColor="#864c28";
+
+    let bubble7=document.getElementById("allbubbles7");
+    bubble7.style.backgroundColor="#864c28";
+
+    let bubble8=document.getElementById("allbubbles8");
+    bubble8.style.backgroundColor="#864c28";
+
+    let bubble9=document.getElementById("allbubbles9");
+    bubble9.style.backgroundColor="#864c28";
+
+    let bubble10=document.getElementById("allbubbles10");
+    bubble10.style.backgroundColor="#864c28";
+
+    let logobackground=document.getElementById("fancyborder");
+    logobackground.style.backgroundColor="#ffe23c";
+    let logo=document.getElementById("canLogo");
+    logo.src="images/FantaLogo.png";
+    let can=document.getElementById("canImage");
+    can.src="images/fanta 2.png";
+}
+// Sprite
+
+let sprite=document.getElementById("Sprite");
+sprite.addEventListener("click",spr)
+function spr(){
+    document.body.style.backgroundColor = "#57be7e";
+
+    let bubble1=document.getElementById("allbubbles1");
+    bubble1.style.backgroundColor="#1a5e76";
+
+    let bubble2=document.getElementById("allbubbles2");
+    bubble2.style.backgroundColor="#1a5e76";
+
+    let bubble3=document.getElementById("allbubbles3");
+    bubble3.style.backgroundColor="#1a5e76";
+
+    let bubble4=document.getElementById("allbubbles4");
+    bubble4.style.backgroundColor="#1a5e76";
+
+    let bubble5=document.getElementById("allbubbles5");
+    bubble5.style.backgroundColor="#1a5e76";
+
+    let bubble6=document.getElementById("allbubbles6");
+    bubble6.style.backgroundColor="#1a5e76";
+
+    let bubble7=document.getElementById("allbubbles7");
+    bubble7.style.backgroundColor="#1a5e76";
+
+    let bubble8=document.getElementById("allbubbles8");
+    bubble8.style.backgroundColor="#1a5e76";
+
+    let bubble9=document.getElementById("allbubbles9");
+    bubble9.style.backgroundColor="#1a5e76";
+
+    let bubble10=document.getElementById("allbubbles10");
+    bubble10.style.backgroundColor="#1a5e76";
+
+    let logobackground=document.getElementById("fancyborder");
+    logobackground.style.backgroundColor="#00843b";
+    let logo=document.getElementById("canLogo");
+    logo.src="images/SpriteLogo.png";
+    let can=document.getElementById("canImage");
+    can.src="images/sprite can.png";
+}
+
